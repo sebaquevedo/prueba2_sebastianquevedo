10ec3f8bdb74b132deb9594826d999bbd8e7c7b7
sebastian quevedo <sebaqd91@gmail.com>
2017-02-22 12:03:56 -0300

    fifth commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
 	belongs_to :category
 	belongs_to :user, optional: true
+	scope :return_x_size, ->(x){ where(size: x)}
+	scope :return_nil_users, ->{ where(user_id: nil)}
 end
