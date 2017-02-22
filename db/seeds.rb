712604115b226cd4aeec8b32427dcd5d7581a096
sebastian quevedo <sebaqd91@gmail.com>
2017-02-22 13:39:43 -0300

    seed para probar delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.destroy_all
+Category.destroy_all
+
+Category.create(name:'categoria 1')
+Category.create(name:'categoria 2')
+Category.create(name:'categoria 3')
+
+Item.create(serialnumber:23,size:100,description:'soy un item de calidad',category_id:Category.last)
+Item.create(serialnumber:234,size:150,description:'soy un item penca',category_id:Category.last)
+Item.create(serialnumber:34,size:1450,description:'soy un item shuer loco',category_id:Category.last)
